fd8982fc3376754b84a7de492d02b552ab1648ea
eventFan <priyavarshini1504@gmail.com>
2025-05-23 12:16:41 +0530

    Create HELLOWORLD.java

---
+++ COREJAVA/HELLOWORLD.java
@@ -0,0 +1,9 @@
+// HelloWorld.java
+public class HelloWorld {
+
+    // Main method: Entry point of the Java program
+    public static void main(String[] args) {
+        // Print message to the console
+        System.out.println("Hello, World!");
+    }
+}
