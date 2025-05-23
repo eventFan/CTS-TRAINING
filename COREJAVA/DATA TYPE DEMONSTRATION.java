e5200d9274cb2e587ee7189e9a75d267fa68e0bd
eventFan <priyavarshini1504@gmail.com>
2025-05-23 12:21:22 +0530

    Create DATA TYPE DEMONSTRATION.java

---
+++ COREJAVA/DATA TYPE DEMONSTRATION.java
@@ -0,0 +1,32 @@
+// DataTypeDemo.java
+
+public class DataTypeDemo {
+
+    public static void main(String[] args) {
+        // Integer types
+        byte byteVar = 100;
+        short shortVar = 10000;
+        int intVar = 100000;
+        long longVar = 10000000000L;
+
+        // Floating point types
+        float floatVar = 10.5f;
+        double doubleVar = 123.456789;
+
+        // Character type
+        char charVar = 'A';
+
+        // Boolean type
+        boolean boolVar = true;
+
+        // Display values
+        System.out.println("byte: " + byteVar);
+        System.out.println("short: " + shortVar);
+        System.out.println("int: " + intVar);
+        System.out.println("long: " + longVar);
+        System.out.println("float: " + floatVar);
+        System.out.println("double: " + doubleVar);
+        System.out.println("char: " + charVar);
+        System.out.println("boolean: " + boolVar);
+    }
+}
