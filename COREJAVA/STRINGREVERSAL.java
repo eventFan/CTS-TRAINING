add028935cd3aa41cdbfd025081fc9ea1d97365e
eventFan <priyavarshini1504@gmail.com>
2025-05-23 12:36:16 +0530

    Create STRINGREVERSAL.java

---
+++ COREJAVA/STRINGREVERSAL.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringReversal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+
+        // Method 1: Using StringBuilder's reverse() method
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        System.out.println("Reversed string: " + reversed);
+
+        scanner.close();
+    }
+}
+
